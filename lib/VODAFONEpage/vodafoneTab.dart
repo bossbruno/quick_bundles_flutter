513d5ed81d3cead98dac278a18293ab6cfaed8fc
Dennis Kwakye <kwakye105@gmail.com>
2023-12-06 00:02:46 +0000

    added more vodafone buttons

--- lib/VODAFONEpage/vodafoneTab.dart
+++ lib/VODAFONEpage/vodafoneTab.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
-
 class VodafoneScreen extends StatefulWidget {
   const VodafoneScreen({super.key});
 
@@ -13,16 +12,15 @@ class VodafoneScreen extends StatefulWidget {
 
 class _VodafoneScreenState extends State<VodafoneScreen> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return   SizedBox(
+    return SizedBox(
       height: 30,
-      child:
-      Padding(
+      child: Padding(
         padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
         child: Container(
           decoration: const BoxDecoration(
@@ -33,74 +31,163 @@ class _VodafoneScreenState extends State<VodafoneScreen> {
           child: Column(
             children: <Widget>[
               Padding(
-                padding:const EdgeInsets.fromLTRB(1, 20, 1, 1),
-                child: ElevatedButton(onPressed: () {
-                  FlutterPhoneDirectCaller.callNumber("*126#");
-
-                },
-
-
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white,
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*126#");
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
                       foregroundColor: Colors.black,
-
-                      shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       // StadiumBorder(),
 
                       fixedSize: const Size(350, 50)),
-
-                  child:  const Text('VODAFONE CHECK CREDIT BALANCE \n *126#'  ,textAlign: TextAlign.center,), ),
-
-
-
-
+                  child: const Text(
+                    'VODAFONE CHECK CREDIT BALANCE \n *126#',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
               Padding(
-                padding:const EdgeInsets.fromLTRB(1, 20, 1, 1),
-                child: ElevatedButton(onPressed: () {
-                  FlutterPhoneDirectCaller.callNumber("*135*2*1#");
-                },
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*151#");
+                  },
                   // icon:const Icon(Icons.call),
 
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
                       foregroundColor: Colors.black,
-                      shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       // StadiumBorder(),
 
                       fixedSize: const Size(350, 50)),
 
-                  child:  const Text('VODAFONE INFORMATION SERVICE \n *151#'  ,textAlign: TextAlign.center),),
-
-
-
-
+                  child: const Text('VODAFONE INFORMATION SERVICE \n *151#',
+                      textAlign: TextAlign.center),
+                ),
               ),
               Padding(
-                padding:const EdgeInsets.fromLTRB(1, 20, 1, 1),
-                child: ElevatedButton(onPressed: () {
-                  FlutterPhoneDirectCaller.callNumber("*135*2*1#");
-                },
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*126#");
+                  },
                   // icon:const Icon(Icons.call),
 
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
                       foregroundColor: Colors.black,
-                      shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                       // StadiumBorder(),
 
                       fixedSize: const Size(350, 50)),
 
-                  child:  const Text('VODAFONE CASH \n *110#'  ,textAlign: TextAlign.center),),
+                  child: const Text('CHECK BUNDLE BALANCE \n *126#',
+                      textAlign: TextAlign.center),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*110#");
+                  },
+                  // icon:const Icon(Icons.call),
 
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // StadiumBorder(),
 
+                      fixedSize: const Size(350, 50)),
 
+                  child: const Text('VODAFONE CASH \n *110#',
+                      textAlign: TextAlign.center),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*127#");
+                  },
+                  // icon:const Icon(Icons.call),
 
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // StadiumBorder(),
+
+                      fixedSize: const Size(350, 50)),
+
+                  child: const Text('CHECK YOUR NUMBER \n *127#',
+                      textAlign: TextAlign.center),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*700#");
+                  },
+                  // icon:const Icon(Icons.call),
+
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // StadiumBorder(),
+
+                      fixedSize: const Size(350, 50)),
+
+                  child: const Text('VODAFONE INTERNET PACKAGES \n *700#',
+                      textAlign: TextAlign.center),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FlutterPhoneDirectCaller.callNumber("*530#");
+                  },
+                  // icon:const Icon(Icons.call),
+
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // StadiumBorder(),
+
+                      fixedSize: const Size(350, 50)),
+
+                  child: const Text('VODAFONE MADEFORME BUNDLES \n *530#',
+                      textAlign: TextAlign.center),
+                ),
               ),
 
             ],
           ),
-
         ),
-
-
       ),
     );
   }
